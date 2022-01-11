7e513c22d610d54ac5092690732d019070aef534
Dan33311 <60445863+Dan33311@users.noreply.github.com>
2022-01-10 19:59:29 -0500

    create one questions category and styles

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,60 +3,73 @@ const Questions = () => {
   const questions = [
     {
       id: 1,
-      questionText : 'Here is the question #1 ?',
+      questionText : '¿Qué significa CSS?',
       answersOptions: [
-        { option1: 'option 1', isCorrect: false },
-        { option2: 'option 2', isCorrect: false },
-        { option3: 'option 3', isCorrect: true },
-        { option4: 'option 4', isCorrect: false },
+        { answerText: 'Color Style Sheets', isCorrect: false },
+        { answerText: 'Coded Style Sheets', isCorrect: false },
+        { answerText: 'Cascading Style Sheets', isCorrect: true },
+        { answerText: 'Cross-browser Style Sheets', isCorrect: false },
       ]
     },
     {
       id: 2,
-      questionText : 'question2',
+      questionText : '¿Cómo podemos añadir un comentario en una hoja de estilo CSS?',
       answersOptions: [
-        { option1: '', isCorrect: false },
-        { option2: '', isCorrect: false },
-        { option3: '', isCorrect: false },
-        { option4: '', isCorrect: true },
+        { answerText: '/* esto es un comentario CSS */', isCorrect: true },
+        { answerText: '<!-- Esto es un comentario CSS -->', isCorrect: false },
+        { answerText: 'No se puede', isCorrect: false },
+        { answerText: '// Este es un comentario CSS', isCorrect: false },
       ]
     },
     {
       id: 3,
-      questionText : 'question3',
+      questionText : '¿En qué sección de la página HTML podemos definir una hoja de estilo interna CSS?',
       answersOptions: [
-        { option1: '', isCorrect: false },
-        { option2: '', isCorrect: false },
-        { option3: '', isCorrect: true },
-        { option4: '', isCorrect: false },
+        { answerText: 'En la sección <head>', isCorrect: true },
+        { answerText: 'Se puede definir en cualquier sección de la página', isCorrect: false },
+        { answerText: 'En al sección <body>', isCorrect: false },
+        { answerText: 'En la sección <footer>', isCorrect: false },
       ]
     },
     {
       id: 4,
-      questionText : 'question4',
+      questionText : '¿Para que se utiliza la regla @keyframes?',
       answersOptions: [
-        { option1: '', isCorrect: true },
-        { option2: '', isCorrect: false },
-        { option3: '', isCorrect: false },
-        { option4: '', isCorrect: false },
+        { answerText: 'Transición', isCorrect: false },
+        { answerText: 'Rotación', isCorrect: false },
+        { answerText: 'Animación', isCorrect: true },
+        { answerText: 'Importar fuentes', isCorrect: false },
       ]
     },
-    
+    {
+      id: 5,
+      questionText : '¿Cual es la respuesta correcta para generar un efecto de transición?',
+      answersOptions: [
+        { answerText: 'transition: height .8s;', isCorrect: true },
+        { answerText: 'transition-height: .8s;', isCorrect: false },
+        { answerText: 'transform: transition(height, .8s);', isCorrect: false },
+        { answerText: 'transition-s: 8;', isCorrect: false },
+      ]
+    },
   ]
 
 
   return (
     <>
-      <div className="question-count">Question number: _ of {questions.length}</div>
-      <div className="question-text">{questions[0].questionText}</div>
-      <div className="answer-options">
-        <p>{questions[0].answersOptions[0].option1}</p>
-        <p>{questions[0].answersOptions[1].option2}</p>
-        <p>{questions[0].answersOptions[2].option3}</p>
-        <p>{questions[0].answersOptions[3].option4}</p>
+      <div className="questions-head">
+        <div className="question-count">Question number: _ of {questions.length}</div>
+        <div className="question-text">{questions[0].questionText}</div>
+      </div>
+      <div className="answer-options" >
+        {questions[0].answersOptions.map( answerOption => (
+          <div className="answers-btn">
+            <button>{answerOption.answerText}</button>
+          </div>
+        ))}
       </div>
     </>
   );
 }
 
 export default Questions;
+
